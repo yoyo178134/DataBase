64607ebc190832a2209f6c22d5edbbb40dcea5f6
yahi61006 <50323303+yahi61006@users.noreply.github.com>
2019-12-06 03:33:49 +0800

    a

--- kumomo/php/msgRsv.php
+++ kumomo/php/msgRsv.php
@@ -2,9 +2,11 @@
     require_once 'kumomo_connect.php';
     require_once 'functions.php';
     
-    $data = msgRsv($_GET['send_id'], $_GET['receive_id']);
-    if(!empty($data)){
-        echo json_encode($data);
+    $datas = msgRsv($_GET['send_id'], $_GET['receive_id']);
+    if(!empty($datas)){
+        foreach($datas as $data){
+            echo json_encode($data);
+        }
     }
     else{
         echo 'false';
