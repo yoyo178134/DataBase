1bd852af73a57daaf0cee000ee6110d45f6a1377
yahi61006 <50323303+yahi61006@users.noreply.github.com>
2019-12-06 00:19:01 +0800

    c

--- kumomo/php/userProfile.php
+++ kumomo/php/userProfile.php
@@ -2,9 +2,14 @@
     require_once 'kumomo_connect.php';
     require_once 'functions.php';
 
-    $profile = userProfile($_GET['id']);
-    if(!empty($profile)){
-        echo "true";
+    $data = userProfile($_GET['id']);
+    if(!empty($data)){
+        
+        echo json_encode($data);
+        
+        /*$profile = array('name'=> $data['name'], 'birthdate'=> $data['birthdate'], 'career'=> $data['career'], 'gender'=> $data['gender']);
+        $profile_json = json_encode($profile);
+        echo($profile_json);*/
     }
     else{
         echo "false";
