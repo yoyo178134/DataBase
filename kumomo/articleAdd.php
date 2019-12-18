b215c657b179f866987fb17d1e7bbbf01036e479
TsuNiWan <tom007880510@gmail.com>
2019-12-18 23:20:06 +0800

    Add new page chatroom

--- kumomo/articleAdd.php
+++ kumomo/articleAdd.php
@@ -39,7 +39,7 @@
                 <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
                 <ul class="right hide-on-med-and-down">
                     <li><a href="article.php">文章</a></li>
-                    <li><a href="chat.php">聊天室</a></li>
+                    <li><a href="chatroom.php">聊天室</a></li>
                     <li><a href="profile.php">個人頁面</a></li>
                     <li><a class="waves-effect waves-light btn" href="php/logout.php">登出</a></li>
                 </ul>
@@ -49,7 +49,7 @@
 
     <ul class="sidenav" id="mobile-demo">
         <li><a href="article.php">文章</a></li>
-        <li><a href="chat.php">聊天室</a></li>
+        <li><a href="chatroom.php">聊天室</a></li>
         <li><a href="profile.php">個人頁面</a></li>
         <li><a class="waves-effect waves-light btn" href="php/logout.php">登出</a></li>
     </ul>
