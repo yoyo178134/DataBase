3bf091205659fbd967a27856c5bcfbf9ce1dcaf2
yoyo <yoyo17813@gmail.com>
2019-12-19 11:37:17 +0800

    ?

--- kumomo/article.php
+++ kumomo/article.php
@@ -168,9 +168,9 @@
                         temp.find("#likes").attr("class","like"+id)
                         temp.find("#delete").attr("class","delBut"+id)
                         temp.find("#thumb").attr('onclick',"thumbclick(" + id + ")");
-                        console.log("artid"+ele.id+" postid: "+ele.poster_id+" myid: "+userid)
+                        
                         if(ele.poster_id === userid){
-                            
+                            console.log("artid"+ele.id+" postid: "+ele.poster_id+" myid: "+userid)
                             temp.find("#delete").attr('onclick',"deleteOnclick(" + id  + ")");
                             temp.find("#delete").parent().show();
                         }
