347e295a9743e031407af36357a1014d21073a19
yahi61006 <yahi61006@gmail.com>
2019-12-20 16:44:37 +0800

    Change findAccont to findId

--- kumomo/php/findId.php
+++ kumomo/php/findId.php
@@ -2,7 +2,7 @@
     require_once 'kumomo_connect.php';
     require_once 'functions.php';
 
-    $id = findAccount($_GET['account']);
+    $id = findId($_GET['account']);
 
     if($id!=null){
         echo $id;
