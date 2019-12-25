e6550ae9fdf933db89aba46dfe0d656230587073
TsuNiWan <tom007880510@gmail.com>
2019-12-25 23:56:06 +0800

    name remove

--- kumomo/chat.php
+++ kumomo/chat.php
@@ -74,7 +74,6 @@
         <br><br>
         <div class="messageList">
             <div id="recvId" class="hide"><?php echo $id ?></div>
-            <div id="recvName" class="hide"><?php echo $name ?></div>
             <div class="messageBox left">
                 <p>123</p>
             </div>
