487d8624e75501623ba17d418d733b1554208be1
TsuNiWan <tom007880510@gmail.com>
2019-12-26 00:22:00 +0800

    Update message box

--- kumomo/chat.php
+++ kumomo/chat.php
@@ -84,15 +84,15 @@
         <div class="messageList">
             <div id="recvId" class="hide"><?php echo $id ?></div>
             <div class="row">
-                <div class="col s6 m4"> 
-                    <div class="messageBox left">
+                <div class="col s6 m4 left"> 
+                    <div class="messageBox">
                         <span>123</span>
                     </div>
                 </div>
             </div>
             <div class="row">
-                <div class="col s6 m4">
-                    <div class="messageBox right"> 
+                <div class="col s6 m4 right">
+                    <div class="messageBox"> 
                         <span>45jhfjsdhfdslhfiushdiulhasliujfnjfnjnjfnvjfnvjnfjvnfjnvfjnvjfvjfnvjfnvjfnvjfnjekdnwkdjkwmdmdwkdiuashdliuhasuil6</span>
                     </div>
                 </div>
