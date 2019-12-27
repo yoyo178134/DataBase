3e573341b59ee5ac7e53d2ac5b127e4506ced497
yahi61006 <50323303+yahi61006@users.noreply.github.com>
2019-12-27 22:56:50 +0800

    Update server

--- kumomo/php/server.php
+++ kumomo/php/server.php
@@ -182,7 +182,7 @@
         if($carrerUsers){//有對應職業的user
             foreach($carrerUsers as $carrerUser){
                 $userKey = findReceiverKey($carrerUser['id']);
-                $msg_ar3 = array('text'=>'['.findIDtoAccount($temp_msg->send_id).']'.findName($temp_msg->send_id)."\n".$temp_msg->text, 'send_id'=> $carrerUser['id'], 'receive_id'=> $temp_msg->receive_id, 
+                $msg_ar3 = array('text'=>'['.findIDtoAccount($temp_msg->send_id).']'.findName($temp_msg->send_id).':'."\n".$temp_msg->text, 'send_id'=> $carrerUser['id'], 'receive_id'=> $temp_msg->receive_id, 
                                 'time'=>date("Y-m-d H:i:s"), 'isRead'=>1, 'isOwner'=> 0);
                 $str3 = msg_encode(json_encode($msg_ar3));
                 msgSend($temp_msg->text, $temp_msg->receive_id, $carrerUser['id']);//MySQL
