6be558a8955410dd5869739707498606f0ad0a3c
YOYO <yoyo17813@gmail.com>
2019-12-27 18:01:53 +0800

    recv name

--- kumomo/chat.php
+++ kumomo/chat.php
@@ -240,7 +240,24 @@
                 var Data = JSON.parse(event.data);
                 console.log(Data);
                 if(recvid == Data.receive_id){
-                    writeMessage(Data.text,Data.time,(Data.isOwner == "1"))
+                    var recvName;
+                    $.ajax({
+                        type: "GET",
+                        url: "php/findName.php",
+                        dataType : "text",
+                        data: {
+                        id : recvid
+                        },
+                        success: function (data) {
+                            recvName = date;
+                        },
+                        error: function(XMLHttpRequest, textStatus, errorThrown) {
+                            alert(XMLHttpRequest.status);
+                            alert(XMLHttpRequest.readyState);
+                            alert(textStatus);
+                        }
+                    })
+                    writeMessage(recvName+" : "+Data.text,Data.time,(Data.isOwner == "1"))
                     $(".messageList").animate({scrollTop:$(".messageList").get(0).scrollHeight});
                 }else{
                     M.toast({html: Data.text, displayLength: 2000, completeCallback: function () { }})
